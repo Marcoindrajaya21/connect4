98d1a14c25ff379a6e509e329f473058e860257d
Marcoindrajaya21 <162570609+Marcoindrajaya21@users.noreply.github.com>
2024-12-20 22:27:09 +0700

    Update ConnectFour.java

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -1,5 +1,16 @@
 package connectfour;
 
+
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231019 - Nathaniel Lado Hadi Winata
+ * 2 - 5026231031 - Marco Indrajaya
+ */
+
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
