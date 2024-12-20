042bf32aeb2ce1533381b856fd4d5e69293e702e
Marcoindrajaya21 <162570609+Marcoindrajaya21@users.noreply.github.com>
2024-12-20 22:26:20 +0700

    Update AIPlayer.java

--- src/AIPlayer.java
+++ src/AIPlayer.java
@@ -1,4 +1,13 @@
 package connectfour;
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231019 - Nathaniel Lado Hadi Winata
+ * 2 - 5026231031 - Marco Indrajaya
+ */
+
 
 import java.util.ArrayList;
 import java.util.Random;
